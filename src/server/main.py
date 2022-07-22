a17175077fbb54179b9567604b3dda79d56c5312
John L. Jolly <mirthfather@gmail.com>
2022-07-22 21:14:32 +0000

    Add simple server

---
+++ src/server/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import asyncio
+
+import websockets
+
+
+async def game_server(sock):
+    """Main gameserver message handler"""
+    async for msg in sock:
+        await sock.send("Not implemented")
+
+
+async def main():
+    """Main websocket event loop"""
+    async with websockets.serve(game_server, "0.0.0.0", 7890):
+        await asyncio.Future()
+
+if __name__ == "__main__":
+    asyncio.run(main())
