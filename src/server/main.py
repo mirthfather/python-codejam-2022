f2d12d12e1881c030fd1e48cc97e3784f841b882
John L. Jolly <mirthfather@gmail.com>
2022-07-31 03:15:45 +0000

    [server/main.py]: Add message echo delay of 1000ms

--- src/server/main.py
+++ src/server/main.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 import asyncio
+from datetime import datetime as dt
 
+# import pygame
 import websockets
+import json
+
+history = []
 
 
 async def game_server(sock):
     """Main gameserver message handler"""
+    print(f'Connection: {sock}')
     async for msg in sock:
-        await sock.send("Not implemented")
+        tr = dt.now()
+        history.append({
+            "time_recv": tr,
+            "message": msg})
+        ret = []
+        while len(history) > 0 and (tr - history[0]["time_recv"]).seconds >= 1:
+            ret.append(history[0]["message"])
+            history.pop(0)
+
+        await sock.send(json.dumps(ret))
 
 
 async def main():
